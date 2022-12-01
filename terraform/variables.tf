c99865f5b8d30b0fb38a67a77b004f43ffe3f600
Sammywond23 <sammywond1@gmail.com>
2022-12-01 10:23:52 -0500

    Init Repo

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,8 @@
-variable "resource_groups" {
-  type = map(object({
-    location = string
-  }))
-}
+variable "bootcamp_resource_group_name" {
+  type = string
+  description = "I am a resource group name"
+  }
+  variable "bootcamp_resource_group_location" {
+  type = string
+  description = "I am a resource group name"
+  }
